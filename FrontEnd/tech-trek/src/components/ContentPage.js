f36a934f2417ae888daf8d92e3d737e1bb5279be
liyanang <liyananggg@gmail.com>
2022-03-25 11:31:04 +0800

    Frontend template

---
+++ FrontEnd/tech-trek/src/components/ContentPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/example.css";
+
+function ContentPage() {
+  return (
+    <div>
+      <h1>Content Page</h1>
+    </div>
+  );
+}
+
+export default ContentPage;
